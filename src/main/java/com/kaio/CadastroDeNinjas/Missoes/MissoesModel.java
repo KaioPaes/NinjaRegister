1ff7ed68020330be3774e716a53622eba284ddb8
Jonny <164424346+KaioPaes@users.noreply.github.com>
2026-01-30 01:08:28 -0300

    Refatoração utilizando Lombok para criação dos constructors, getters e setters

--- src/main/java/com/kaio/CadastroDeNinjas/Missoes/MissoesModel.java
+++ src/main/java/com/kaio/CadastroDeNinjas/Missoes/MissoesModel.java
@@ -2,17 +2,24 @@ package com.kaio.CadastroDeNinjas.Missoes;
 
 import com.kaio.CadastroDeNinjas.Ninjas.NinjaModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
 @Table(name = "tb_missoes")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class MissoesModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String dificulty;
+
     //Um ninja pode ter somente uma missão por vez
     @OneToMany(mappedBy = "missions")
     private List<NinjaModel> ninja;
--- src/main/java/com/kaio/CadastroDeNinjas/Ninjas/NinjaModel.java
+++ src/main/java/com/kaio/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -2,11 +2,17 @@ package com.kaio.CadastroDeNinjas.Ninjas;
 
 import com.kaio.CadastroDeNinjas.Missoes.MissoesModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
 @Table(name = "tb_cadastro")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class NinjaModel {
 
     @Id
@@ -20,37 +26,4 @@ public class NinjaModel {
     @ManyToOne()
     @JoinColumn(name = "missions_id")
     private MissoesModel missions;
-
-    public NinjaModel() {
-    }
-
-    public NinjaModel(String nome, String email, int age) {
-        this.nome = nome;
-        this.email = email;
-        this.age = age;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
 }
